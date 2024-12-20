3b8799d7f2986b7fa79e2b40ab97d24093a73b9a
bhumikakarri <karrisarika2004@gmail.com>
2024-12-20 14:50:12 +0530

    new commits

--- src/Components/Module/Headnav.jsx
+++ src/Components/Module/Headnav.jsx
@@ -31,7 +31,7 @@ function Headernav({ setSelectedBrand }) {
             <div className='d-flex gap-2'>
             <Link to='/dashboard'><p>Back</p></Link> 
             <div className='logout'>
-<button type="button" class="btn btn-outline-primary" ><Link to={'/'}>Logout</Link></button>
+<button type="button" class="btn btn-outline-primary" ><Link to={'/Reactwork'}>Logout</Link></button>
       </div>
       </div>
           </Nav>
